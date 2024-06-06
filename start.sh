f5f61dabd001e84b67f5f753688bb056928c3403
Avgust work pc <avgustfavn@yandex.ru>
2024-06-06 08:17:16 +0300

    New xml

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Активируем виртуальное окружение
-source /Dylyherb_parser/venv/bin/activate
-
-# Запускаем файл Python
-python /Dylyherb_parser/main.py > /Dylyherb_parser/script.log
-
-# Деактивируем в
